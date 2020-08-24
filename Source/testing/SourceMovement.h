4b8511d84317990f908769aa70ba3cd6a652377c
dellevelled <delle.caj@gmail.com>
2020-08-24 14:07:38 -0400

    Convert to C++ project

---
+++ Source/testing/SourceMovement.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class TESTING_API SourceMovement
+{
+public:
+	SourceMovement();
+	~SourceMovement();
+};
---
+++ Source/testing/testing.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "testing.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, testing, "testing" );
